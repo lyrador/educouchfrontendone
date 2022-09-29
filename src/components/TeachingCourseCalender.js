fe723d73d05a3cc630ef98a13616a35416db93d4
sachok123 <e0540367@u.nus.edu>
2022-09-29 16:22:08 +0800

    display current appointments on calender

--- src/components/TeachingCourseCalender.js
+++ src/components/TeachingCourseCalender.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import * as React from "react";
 import  { ViewState } from '@devexpress/dx-react-scheduler';
 import { EditingState } from '@devexpress/dx-react-scheduler';
 import { IntegratedEditing } from '@devexpress/dx-react-scheduler';
@@ -7,11 +8,27 @@ import { Scheduler, WeekView, Appointments, AppointmentForm } from '@devexpress/
 
 
 const TeachingCourseCalender = () => {
+
+const [events, setEvents] = useState([])
+
+React.useEffect(() => {
+    fetch("http://localhost:8080/event/events").
+        then(res => res.json()).then((result) => {
+            setEvents(result);
+        }
+        )
+}, [])
+
+const saveAppointment = (data) => {
+    console.log(data)
+}
+
+console.log(events); 
     return (
         <div id="calender">
-            <Scheduler>
+            <Scheduler data={events}>
                 <ViewState />
-                <EditingState />
+                <EditingState onCommitChanges={saveAppointment}/>
                 <IntegratedEditing />
                 <WeekView />
                 <Appointments />
