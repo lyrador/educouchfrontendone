c43e55dac4f4b6619280bfa90df3f8239902f181
sachok123 <e0540367@u.nus.edu>
2022-10-04 11:29:38 +0800

    added in create, retrieve and update events on calender

--- src/components/TeachingCourseCalender.js
+++ src/components/TeachingCourseCalender.js
@@ -3,15 +3,59 @@ import * as React from "react";
 import  { ViewState } from '@devexpress/dx-react-scheduler';
 import { EditingState } from '@devexpress/dx-react-scheduler';
 import { IntegratedEditing } from '@devexpress/dx-react-scheduler';
-import { Scheduler, WeekView, Appointments, AppointmentForm } from '@devexpress/dx-react-scheduler-material-ui';
+import { Scheduler, WeekView, Appointments, AppointmentForm, Toolbar, DateNavigator, TodayButton} from '@devexpress/dx-react-scheduler-material-ui';
 
 
 
 const TeachingCourseCalender = () => {
 
 const [events, setEvents] = useState([])
+const views = ['timelineDay', 'timelineWeek', 'timelineWorkWeek', 'timelineMonth'];
+const currentDate = new Date();
 
+const [editingAppointment, setEditingAppointment] = useState('');
+const [addedAppointment, setAddedAppointment] = useState({})
+const [appointmentChanges, setAppointmentChanges] = useState({})
+
+const [refreshPage, setRefreshPage] = useState('')
+
+const changeAddedAppointment = (addedAppointment) => {
+    setAddedAppointment(addedAppointment); 
+}
+
+const changeAppointmentChanges = (appointmentChanges) => {
+    setAppointmentChanges(appointmentChanges); 
+}
+
+const changeEditingAppointment = (editingAppointment) => {
+     setEditingAppointment(editingAppointment); 
+}
+
+
+
+const commitChanges = ({added, changed}) => {
+    
+    if (changed) {
+
+        var title = (appointmentChanges.title === undefined) ? editingAppointment.title : appointmentChanges.title
+        var startDate = (appointmentChanges.startDate === undefined) ? editingAppointment.startDate : appointmentChanges.startDate
+        var endDate = (appointmentChanges.endDate === undefined) ? editingAppointment.endDate : appointmentChanges.endDate
+        var notes = (appointmentChanges.notes == undefined) ? editingAppointment.notes : appointmentChanges.notes
+        var allDay = (appointmentChanges.allDay == undefined) ? editingAppointment.allDay : appointmentChanges.allDay
+        var id = editingAppointment.id
+        const appointment1 = {title, startDate, endDate, notes, allDay, id}; 
+        editAppointment(appointment1)
+    }
+
+    if (added) {
+        //const startingAddedId = events1.length > 0 ? events1[events1.length - 1].id + 1 : 0;
+        //events1 = [...events1, { id: startingAddedId, ...added }];
+        saveAppointment(addedAppointment)
+    }
+}
+  
 React.useEffect(() => {
+    setRefreshPage(false);
     fetch("http://localhost:8080/event/events").
         then(res => res.json()).then((result) => {
             setEvents(result);
@@ -21,16 +65,56 @@ React.useEffect(() => {
 
 const saveAppointment = (data) => {
     console.log(data)
+    fetch("http://localhost:8080/event/events" , {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(data)
+        }).then(() => {
+            console.log("Event Created Successfully!")
+            setRefreshPage(true)
+        
+        })
+}
+
+const editAppointment = (data) => {
+    console.log(data)
+    fetch("http://localhost:8080/event/events/" + data.id , {
+            method: "PUT",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(data)
+        }).then(() => {
+            console.log("Event Updated Successfully!")
+            setRefreshPage(true)
+        
+        })
 }
 
 console.log(events); 
     return (
         <div id="calender">
-            <Scheduler data={events}>
+            <Scheduler 
+                timeZone="Asia/Singapore"
+                data={events}
+                views={views}
+                defaultCurrentView="timelineDay"
+                defaultCurrentDate={currentDate}
+                firstDayOfWeek={0}
+                >
                 <ViewState />
-                <EditingState onCommitChanges={saveAppointment}/>
-                <IntegratedEditing />
                 <WeekView />
+                <EditingState 
+                    onCommitChanges={commitChanges}
+                    addedAppointment = {addedAppointment}
+                    onAddedAppointmentChange={changeAddedAppointment}
+                    appointmentChanges={appointmentChanges}
+                    onAppointmentChangesChange={changeAppointmentChanges}
+                    editingAppointment = {editingAppointment}
+                    onEditingAppointmentChange = {changeEditingAppointment}
+                />
+                <IntegratedEditing />
+                <Toolbar />
+                <DateNavigator />
+                <TodayButton />
                 <Appointments />
                 <AppointmentForm />
             </Scheduler>
