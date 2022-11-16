3f50a93aebe866fc05f5fb93b6e9d5dfc6601b98
irene <irenelie1412@gmail.com>
2022-11-16 22:08:04 +0800

    stuff

--- src/components/GardenComponents/HomepageGarden.js
+++ src/components/GardenComponents/HomepageGarden.js
@@ -7,6 +7,35 @@ import { useNavigate } from "react-router-dom"
 
 function HomepageGarden() {
 
+    const [linkOneOne, setLinkOneOne] = useState("");
+    const [linkOneTwo, setLinkOneTwo] = useState("");
+    const [linkOneThree, setLinkOneThree] = useState("");
+    const [linkOneFour, setLinkOneFour] = useState("");
+    const [linkOneFive, setLinkOneFive] = useState("");
+
+    const [linkTwoOne, setLinkTwoOne] = useState("");
+    const [linkTwoTwo, setLinkTwoTwo] = useState("");
+    const [linkTwoThree, setLinkTwoThree] = useState("");
+    const [linkTwoFour, setLinkTwoFour] = useState("");
+    const [linkTwoFive, setLinkTwoFive] = useState("");
+
+    const [linkThreeOne, setLinkThreeOne] = useState("");
+    const [linkThreeTwo, setLinkThreeTwo] = useState("");
+    const [linkThreeThree, setLinkThreeThree] = useState("");
+    const [linkThreeFour, setLinkThreeFour] = useState("");
+    const [linkThreeFive, setLinkThreeFive] = useState("");
+
+    const [linkFourOne, setLinkFourOne] = useState("");
+    const [linkFourTwo, setLinkFourTwo] = useState("");
+    const [linkFourThree, setLinkFourThree] = useState("");
+    const [linkFourFour, setLinkFourFour] = useState("");
+    const [linkFourFive, setLinkFourFive] = useState("");
+
+    const [linkFiveOne, setLinkFiveOne] = useState("");
+    const [linkFivewo, setLinkFiveTwo] = useState("");
+    const [linkFiveThree, setLinkFiveThree] = useState("");
+    const [linkFiveFour, setLinkFiveFour] = useState("");
+    const [linkFiveFive, setLinkFiveFive] = useState("");
 
 
     return (
